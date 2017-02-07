d1a02e521101dcd77de7eca8ae62e6d212f696fb
ice-leng <xykxyk2008@163.com>
2017-02-07 21:42:04 +0800

    添加  全局 cache

--- api/business/common/Init.php
+++ api/business/common/Init.php
@@ -14,6 +14,13 @@
 class Init
 {
 
+    /**
+     * 文件读取
+     *
+     * @param array $dirName 目标目录
+     *
+     * @return array [ xxxx=>xxx ]
+     */
     protected static function getFileNames($dirName)
     {
         $rootDir = Yii::getAlias('@business');
@@ -24,18 +31,45 @@ class Init
         return $readDir->getFileNames();
     }
 
+    /**
+     *
+     * 文件缓存 服务
+     *
+     * @return object
+     */
+    private static function _cacheService()
+    {
+        return \Yii::$container->get('yii\caching\FileCache', [], ['cachePath' => '@common/cache']);
+    }
+
+
+    /**
+     *  run
+     */
     public static function run()
     {
         // di 服务
         self::di();
         // event 服务
 
+        // 系统字典
+        self::dist();
     }
 
+    /**
+     * di
+     */
     public static function di()
     {
+        // 先从 di  读取， 如果没有 读取缓存， 还是没有，读取文件
         $impls = require 'map/di.php';
-        if( empty( $impls ) ) $impls = self::getFileNames(['impl']);
+        if( !empty( $impls ) ){
+            $impls = self::_cacheService()->get('_common_cache_di');
+            if( empty( $impls ) ) {
+                $impls = self::getFileNames(['impl']);
+                self::_cacheService()->set('_common_cache_di', $impls);
+            }
+        }
         foreach( $impls as $name => $impl ){
             $interfaces = class_implements($impl);
             \Yii::$container->set($name, $impl);
@@ -44,6 +78,15 @@ class Init
             }
         }
     }
+
+    /**
+     *  字典
+     */
+    public static function dist()
+    {
+
+    }
+
 }
 
 Init::run();
--- api/common/config/main.php
+++ api/common/config/main.php
@@ -10,5 +10,11 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        // 全局缓存
+        'commonCache'=>[
+            'class' => 'yii\caching\FileCache',
+            'cachePath'=>'@common/cache'
+        ],
+
     ],
 ];
